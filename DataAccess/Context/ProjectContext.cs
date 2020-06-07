cca44633b11d1038137889c781a196d07de67735
Baris YEDIPARMAK <b.yediparmak@bilgeadam.com>
2020-06-07 16:39:36 +0300

    Created migration

---
+++ DataAccess/Context/ProjectContext.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Context
+{
+    public class ProjectContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=MININT-GEV2U3F\SQLEXPRESS;Database=ArticleDb;Trusted_Connection=true");
+        }
+
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
