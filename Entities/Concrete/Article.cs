ee417fbc6728454346e2dd29c0c72a2310d37367
Baris YEDIPARMAK <b.yediparmak@bilgeadam.com>
2020-06-07 16:12:41 +0300

    Created Entities

---
+++ Entities/Concrete/Article.cs
@@ -0,0 +1,17 @@
+﻿using Core.Abstract.Interfaces;
+using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Article : BaseEntity, IEntity
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string ImagePath { get; set; }
+
+        public virtual List<Comment> Comments { get; set; }
+    }
+}
---
+++ Entities/Concrete/Comment.cs
@@ -0,0 +1,16 @@
+﻿using Core.Abstract.Interfaces;
+using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Comment : BaseEntity, IEntity
+    {
+        public string Content { get; set; }
+
+        public int ArticleId { get; set; }
+        public virtual Article Article { get; set; }
+    }
+}
