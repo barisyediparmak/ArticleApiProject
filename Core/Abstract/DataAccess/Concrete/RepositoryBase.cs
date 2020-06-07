eb185fd827079b8fe75a3db5a0d8bcdf4ddd6f70
Baris YEDIPARMAK <b.yediparmak@bilgeadam.com>
2020-06-07 17:21:46 +0300

    RepositoryBase created and writed

---
+++ Core/Abstract/DataAccess/Concrete/RepositoryBase.cs
@@ -0,0 +1,69 @@
+﻿using Core.Abstract.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Core.Abstract.DataAccess.Concrete
+{
+    public class RepositoryBase<TEntity, TContext> : IRepository<TEntity>
+        where TEntity : class, IEntity
+        where TContext : DbContext, new()
+    {
+        public void Add(TEntity entity)
+        {
+            using (var context = new TContext())
+            {
+                context.Set<TEntity>().Add(entity);
+                Save();
+            }
+        }
+
+        public List<TEntity> GetAll(Expression<Func<TEntity, bool>> exp = null)
+        {
+            using (var context = new TContext())
+            {
+                return exp == null ?
+                    context.Set<TEntity>().ToList() :
+                    context.Set<TEntity>().Where(exp).ToList();
+            }
+        }
+
+        public TEntity GetByDefault(Expression<Func<TEntity, bool>> exp)
+        {
+            using (var context = new TContext())
+            {
+                return context.Set<TEntity>().FirstOrDefault(exp);
+            }
+        }
+
+        public void Remove(TEntity entity)
+        {
+            using (var context = new TContext())
+            {
+                context.Set<TEntity>().Remove(entity);
+                Save();
+            }
+        }
+
+        public int Save()
+        {
+            using (var context = new TContext())
+            {
+                var result = context.SaveChanges();
+                return result;
+            }
+        }
+
+        public void Update(TEntity entity)
+        {
+            using (var context = new TContext())
+            {
+                context.Set<TEntity>().Update(entity);
+                Save();
+            }
+        }
+    }
+}
--- Core/Abstract/DataAccess/IRepository.cs
+++ Core/Abstract/DataAccess/IRepository.cs
@@ -9,9 +9,8 @@ namespace Core.Abstract.DataAccess
     public interface IRepository<T> where T : class, IEntity
     {
         void Add(T entity);
-        void Remove(int id);
+        void Remove(T entity);
         void Update(T entity);
-        T GetByID(int id);
         T GetByDefault(Expression<Func<T, bool>> exp);
         List<T> GetAll(Expression<Func<T, bool>> exp = null);
         int Save();
