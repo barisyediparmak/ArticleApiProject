1e38780b669c0ebda9bd4eeeae923aa6bed56f8a
Baris YEDIPARMAK <b.yediparmak@bilgeadam.com>
2020-06-07 16:50:24 +0300

    Created Repository Pattern

---
+++ Core/Abstract/DataAccess/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using Core.Abstract.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Core.Abstract.DataAccess
+{
+    public interface IRepository<T> where T : class, IEntity
+    {
+        void Add(T entity);
+        void Remove(int id);
+        void Update(T entity);
+        T GetByID(int id);
+        T GetByDefault(Expression<Func<T, bool>> exp);
+        List<T> GetAll(Expression<Func<T, bool>> exp = null);
+        int Save();
+    }
+}
